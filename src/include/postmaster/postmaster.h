2ccc50eaff1d21d18eaa25388b28b64ecfd059cf
Alexander Korotkov <akorotkov@postgresql.org>
2021-12-13 02:08:21 +0300

    Add convenience functions
    IsFatalError()
    have_backup_in_progress()
    SnapBuildNextPhaseAt()
    DoLocalLockExist()

--- src/include/postmaster/postmaster.h
+++ src/include/postmaster/postmaster.h
@@ -50,6 +50,7 @@ extern PGDLLIMPORT int postmaster_alive_fds[2];
 
 extern PGDLLIMPORT const char *progname;
 
+extern bool IsFatalError(void);
 extern void PostmasterMain(int argc, char *argv[]) pg_attribute_noreturn();
 extern void ClosePostmasterPorts(bool am_syslogger);
 extern void InitProcessGlobals(void);
