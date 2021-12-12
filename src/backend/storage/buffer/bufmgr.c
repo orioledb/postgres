da88dffad4a1bca102c80f66272c8fd6cac0d905
Alexander Korotkov <akorotkov@postgresql.org>
2021-12-13 02:01:16 +0300

    Recovery and checkpointer hooks

--- src/backend/storage/buffer/bufmgr.c
+++ src/backend/storage/buffer/bufmgr.c
@@ -3086,6 +3086,7 @@ BufferSync(int flags)
 		BufferDesc *bufHdr = NULL;
 		CkptTsStatus *ts_stat = (CkptTsStatus *)
 			DatumGetPointer(binaryheap_first(ts_heap));
+		double progress;
 
 		buf_id = CkptBufferIds[ts_stat->index].buf_id;
 		Assert(buf_id != -1);
@@ -3140,7 +3141,10 @@ BufferSync(int flags)
 		 *
 		 * (This will check for barrier events even if it doesn't sleep.)
 		 */
-		CheckpointWriteDelay(flags, (double) num_processed / num_to_scan);
+		progress = (double) num_processed / num_to_scan;
+		progress = CheckPointProgress + progress * (1 - CheckPointProgress);
+
+		CheckpointWriteDelay(flags, progress);
 	}
 
 	/*
