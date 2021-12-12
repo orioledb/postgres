6fca3bcb80f6afd5bc2f76fd620cfc4c5e40e9d4
Alexander Korotkov <akorotkov@postgresql.org>
2021-12-13 01:51:03 +0300

    Snapshot extension and hooks

--- src/backend/access/transam/xact.c
+++ src/backend/access/transam/xact.c
@@ -208,6 +208,7 @@ typedef struct TransactionStateData
 	int			parallelModeLevel;	/* Enter/ExitParallelMode counter */
 	bool		chain;			/* start a new block after this one */
 	bool		topXidLogged;	/* for a subxact: is top-level XID logged? */
+	CommitSeqNo	csn;
 	struct TransactionStateData *parent;	/* back link to parent */
 } TransactionStateData;
 
@@ -241,6 +242,7 @@ static TransactionStateData TopTransactionStateData = {
 	.state = TRANS_DEFAULT,
 	.blockState = TBLOCK_DEFAULT,
 	.topXidLogged = false,
+	.csn = COMMITSEQNO_INPROGRESS
 };
 
 /*
@@ -2003,6 +2005,7 @@ StartTransaction(void)
 	 */
 	s->state = TRANS_START;
 	s->fullTransactionId = InvalidFullTransactionId;	/* until assigned */
+	s->csn = COMMITSEQNO_INPROGRESS;
 
 	/* Determine if statements are logged in this transaction */
 	xact_is_sampled = log_xact_sample_rate != 0 &&
@@ -2277,7 +2280,9 @@ CommitTransaction(void)
 	 * must be done _before_ releasing locks we hold and _after_
 	 * RecordTransactionCommit.
 	 */
+	MyProc->lastCommittedCSN = s->csn;
 	ProcArrayEndTransaction(MyProc, latestXid);
+	s->csn = MyProc->lastCommittedCSN;
 
 	/*
 	 * This is all post-commit cleanup.  Note that if an error is raised here,
@@ -6247,4 +6252,10 @@ xact_redo(XLogReaderState *record)
 	}
 	else
 		elog(PANIC, "xact_redo: unknown op code %u", info);
+}
+
+CommitSeqNo
+GetCurrentCSN(void)
+{
+	return TopTransactionStateData.csn;
 }
