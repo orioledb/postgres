678828b9de38170e5cd7b3f8946c9a2e0de62e4c
Alexander Korotkov <akorotkov@postgresql.org>
2021-12-12 20:36:18 +0300

    Custom TOAST

--- contrib/test_decoding/test_decoding.c
+++ contrib/test_decoding/test_decoding.c
@@ -578,7 +578,7 @@ tuple_to_stringinfo(StringInfo s, TupleDesc tupdesc, HeapTuple tuple, bool skip_
 		/* print data */
 		if (isnull)
 			appendStringInfoString(s, "null");
-		else if (typisvarlena && VARATT_IS_EXTERNAL_ONDISK(origval))
+		else if (typisvarlena && (VARATT_IS_EXTERNAL_ONDISK(origval) || VARATT_IS_EXTERNAL_ORIOLEDB(origval)))
 			appendStringInfoString(s, "unchanged-toast-datum");
 		else if (!typisvarlena)
 			print_literal(s, typid,
