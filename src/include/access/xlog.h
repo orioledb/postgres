a6be12d03cb35765df90dac7ba6c21b876379c3f
Alexander Korotkov <akorotkov@postgresql.org>
2021-12-13 01:51:03 +0300

    Snapshot extension and hooks
    Snapshot have two pairing heap nodes: for data and system undos.

--- src/include/access/xlog.h
+++ src/include/access/xlog.h
@@ -53,6 +53,7 @@ extern PGDLLIMPORT bool track_wal_io_timing;
 extern PGDLLIMPORT int wal_decode_buffer_size;
 
 extern PGDLLIMPORT int CheckPointSegments;
+extern PGDLLIMPORT CommitSeqNo startupCommitSeqNo;
 
 /* Archive modes */
 typedef enum ArchiveMode
