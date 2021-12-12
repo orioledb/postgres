813c22d6f1f510b69ff0cf32cd2d3d02d10f37a6
Alexander Korotkov <akorotkov@postgresql.org>
2021-12-13 02:07:13 +0300

    Allow skipping logging for AccessExclusiveLock

--- src/backend/storage/lmgr/lock.c
+++ src/backend/storage/lmgr/lock.c
@@ -840,7 +840,7 @@ LockAcquireExtended(const LOCKTAG *locktag,
 					LOCALLOCK **locallockp,
 					bool logLockFailure)
 {
-	LOCKMETHODID lockmethodid = locktag->locktag_lockmethodid;
+	LOCKMETHODID lockmethodid;
 	LockMethod	lockMethodTable;
 	LOCALLOCKTAG localtag;
 	LOCALLOCK  *locallock;
@@ -853,6 +853,15 @@ LockAcquireExtended(const LOCKTAG *locktag,
 	bool		found_conflict;
 	ProcWaitStatus waitResult;
 	bool		log_lock = false;
+	bool		no_log_lock = false;
+
+	if (locktag->locktag_lockmethodid == NO_LOG_LOCKMETHOD)
+	{
+		((LOCKTAG *)locktag)->locktag_lockmethodid = DEFAULT_LOCKMETHOD;
+		no_log_lock = true;
+	}
+
+	lockmethodid = locktag->locktag_lockmethodid;
 
 	if (lockmethodid <= 0 || lockmethodid >= lengthof(LockMethods))
 		elog(ERROR, "unrecognized lock method: %d", lockmethodid);
@@ -967,7 +976,8 @@ LockAcquireExtended(const LOCKTAG *locktag,
 	if (lockmode >= AccessExclusiveLock &&
 		locktag->locktag_type == LOCKTAG_RELATION &&
 		!RecoveryInProgress() &&
-		XLogStandbyInfoActive())
+		XLogStandbyInfoActive() &&
+		!no_log_lock)
 	{
 		LogAccessExclusiveLockPrepare();
 		log_lock = true;
