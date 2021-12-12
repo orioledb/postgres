54b8281ed9b7be97b7d18e9e92dd27a085bc2f04
Alexander Korotkov <akorotkov@postgresql.org>
2021-12-13 00:19:41 +0300

    Hook for custom error cleanup

--- src/backend/storage/lmgr/proc.c
+++ src/backend/storage/lmgr/proc.c
@@ -826,6 +826,7 @@ ProcKill(int code, Datum arg)
 	 * facility by releasing our PGPROC ...
 	 */
 	LWLockReleaseAll();
+	CustomErrorCleanup();
 
 	/* Cancel any pending condition variable sleep, too */
 	ConditionVariableCancelSleep();
@@ -935,6 +936,7 @@ AuxiliaryProcKill(int code, Datum arg)
 
 	/* Release any LW locks I am holding (see notes above) */
 	LWLockReleaseAll();
+	CustomErrorCleanup();
 
 	/* Cancel any pending condition variable sleep, too */
 	ConditionVariableCancelSleep();
