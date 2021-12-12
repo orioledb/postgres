8c828b6d0ab2d82eea8f623ac8885e462c75a2fe
Alexander Korotkov <akorotkov@postgresql.org>
2021-12-13 01:51:03 +0300

    Snapshot extension and hooks
    Snapshot have two pairing heap nodes: for data and system undos.

--- src/include/storage/procarray.h
+++ src/include/storage/procarray.h
@@ -100,4 +100,6 @@ extern void ProcArraySetReplicationSlotXmin(TransactionId xmin,
 extern void ProcArrayGetReplicationSlotXmin(TransactionId *xmin,
 											TransactionId *catalog_xmin);
 
+extern snapshot_hook_type snapshot_hook;
+
 #endif							/* PROCARRAY_H */
