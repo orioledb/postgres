54384a84f892adea592aae2ce3c838fe2d9c616c
Alexander Korotkov <akorotkov@postgresql.org>
2021-12-13 01:51:03 +0300

    Snapshot extension and hooks

--- src/backend/storage/ipc/procarray.c
+++ src/backend/storage/ipc/procarray.c
@@ -308,6 +308,8 @@ static GlobalVisState GlobalVisTempRels;
  */
 static TransactionId ComputeXidHorizonsResultLastXmin;
 
+snapshot_hook_type snapshot_hook = NULL;
+
 #ifdef XIDCACHE_DEBUG
 
 /* counters for XidCache measurement */
@@ -754,6 +756,7 @@ ProcArrayEndTransactionInternal(PGPROC *proc, TransactionId latestXid)
 	proc->delayChkptFlags = 0;
 
 	proc->recoveryConflictPending = false;
+	proc->lastCommittedCSN = pg_atomic_fetch_add_u64(&ShmemVariableCache->nextCommitSeqNo, 1);
 
 	/* must be cleared with xid/xmin: */
 	/* avoid unnecessarily dirtying shared cachelines */
@@ -2296,6 +2299,8 @@ GetSnapshotData(Snapshot snapshot)
 
 	if (GetSnapshotDataReuse(snapshot))
 	{
+		if (snapshot_hook)
+			snapshot_hook(snapshot);
 		LWLockRelease(ProcArrayLock);
 		return snapshot;
 	}
@@ -2476,6 +2481,9 @@ GetSnapshotData(Snapshot snapshot)
 
 	if (!TransactionIdIsValid(MyProc->xmin))
 		MyProc->xmin = TransactionXmin = xmin;
+
+	if (snapshot_hook)
+		snapshot_hook(snapshot);
 
 	LWLockRelease(ProcArrayLock);
 
