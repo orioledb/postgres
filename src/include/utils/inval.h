aa5edd91d7f7be31bc904ce451f4c1bee39c0461
Alexander Korotkov <akorotkov@postgresql.org>
2021-12-12 05:24:57 +0300

    CacheInvalidateRelcacheByDbidRelid()

--- src/include/utils/inval.h
+++ src/include/utils/inval.h
@@ -49,6 +49,8 @@ extern void CacheInvalidateRelcacheByTuple(HeapTuple classTuple);
 
 extern void CacheInvalidateRelcacheByRelid(Oid relid);
 
+extern void CacheInvalidateRelcacheByDbidRelid(Oid dbid, Oid relid);
+
 extern void CacheInvalidateSmgr(RelFileLocatorBackend rlocator);
 
 extern void CacheInvalidateRelmap(Oid databaseId);
