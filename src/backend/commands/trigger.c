7428051b378abeaf2e523d3268816bdb20ceda7b
Alexander Korotkov <akorotkov@postgresql.org>
2021-12-13 02:01:16 +0300

    Recovery and checkpointer hooks

--- src/backend/commands/trigger.c
+++ src/backend/commands/trigger.c
@@ -6215,8 +6215,17 @@ AfterTriggerSaveEvent(EState *estate, ResultRelInfo *relinfo,
 
 	/* Determine flags */
 	if (!(relkind == RELKIND_FOREIGN_TABLE && row_trigger))
-		new_event.ate_flags = (row_trigger && event == TRIGGER_EVENT_UPDATE) ?
-			AFTER_TRIGGER_2CTID : AFTER_TRIGGER_1CTID;
+	{
+		if (row_trigger && event == TRIGGER_EVENT_UPDATE)
+		{
+			if (relkind == RELKIND_PARTITIONED_TABLE)
+				new_event.ate_flags = AFTER_TRIGGER_CP_UPDATE;
+			else
+				new_event.ate_flags = AFTER_TRIGGER_2CTID;
+		}
+		else
+			new_event.ate_flags = AFTER_TRIGGER_1CTID;
+	}
 
 	switch (event)
 	{
@@ -6469,20 +6478,6 @@ AfterTriggerSaveEvent(EState *estate, ResultRelInfo *relinfo,
 		{
 			if (!list_member_oid(recheckIndexes, trigger->tgconstrindid))
 				continue;		/* Uniqueness definitely not violated */
-		}
-
-		/* Determine flags */
-		if (!(relkind == RELKIND_FOREIGN_TABLE && row_trigger))
-		{
-			if (row_trigger && event == TRIGGER_EVENT_UPDATE)
-			{
-				if (relkind == RELKIND_PARTITIONED_TABLE)
-					new_event.ate_flags = AFTER_TRIGGER_CP_UPDATE;
-				else
-					new_event.ate_flags = AFTER_TRIGGER_2CTID;
-			}
-			else
-				new_event.ate_flags = AFTER_TRIGGER_1CTID;
 		}
 
 		/*
