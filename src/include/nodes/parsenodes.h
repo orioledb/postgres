7fdb30031cc5e9725ac5423a4b06985c485872e8
Alexander Korotkov <akorotkov@postgresql.org>
2021-12-13 00:04:21 +0300

    Improvements to TableAM API

--- src/include/nodes/parsenodes.h
+++ src/include/nodes/parsenodes.h
@@ -1073,6 +1073,7 @@ typedef struct RangeTblEntry
 	int			rellockmode;	/* lock level that query requires on the rel */
 	struct TableSampleClause *tablesample;	/* sampling info, or NULL */
 	Index		perminfoindex;
+	RowRefType	reftype;
 
 	/*
 	 * Fields valid for a subquery RTE (else NULL):
