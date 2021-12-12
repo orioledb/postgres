18b76b873bf5eaff71c2550b34897286818607ec
Alexander Korotkov <akorotkov@postgresql.org>
2021-12-12 20:26:34 +0300

    CommitSeqNo data type

--- src/include/c.h
+++ src/include/c.h
@@ -623,7 +623,7 @@ typedef double float8;
 
 /*
  * Oid, RegProcedure, TransactionId, SubTransactionId, MultiXactId,
- * CommandId
+ * CommandId, CommitSeqNo
  */
 
 /* typedef Oid is in postgres_ext.h */
@@ -653,6 +653,8 @@ typedef uint32 CommandId;
 
 #define FirstCommandId	((CommandId) 0)
 #define InvalidCommandId	(~(CommandId)0)
+
+typedef uint64 CommitSeqNo;
 
 
 /* ----------------
