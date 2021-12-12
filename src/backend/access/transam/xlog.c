3c156a96a156cdf3cd4d69ffd3481aa51cfe7194
Alexander Korotkov <akorotkov@postgresql.org>
2021-12-13 02:01:16 +0300

    Recovery and checkpointer hooks

--- src/backend/access/transam/xlog.c
+++ src/backend/access/transam/xlog.c
@@ -143,6 +143,11 @@ bool		XLOG_DEBUG = false;
 
 int			wal_segment_size = DEFAULT_XLOG_SEG_SIZE;
 
+/* Hook for plugins to get control in CheckPointGuts() */
+CheckPoint_hook_type CheckPoint_hook = NULL;
+double CheckPointProgress;
+after_checkpoint_cleanup_hook_type after_checkpoint_cleanup_hook = NULL;
+
 /*
  * Number of WAL insertion locks to use. A higher value allows more insertions
  * to happen concurrently, but adds some CPU overhead to flushing the WAL,
@@ -5417,6 +5422,7 @@ StartupXLOG(void)
 	XLogRecPtr	missingContrecPtr;
 	TransactionId oldestActiveXID;
 	bool		promoted = false;
+	bool		wasInRecovery;
 
 	/*
 	 * We should have an aux process resource owner to use, and we should not
@@ -6045,6 +6051,8 @@ StartupXLOG(void)
 	 */
 	PreallocXlogFiles(EndOfLog, newTLI);
 
+	wasInRecovery = InRecovery;
+
 	/*
 	 * Okay, we're officially UP.
 	 */
@@ -6122,6 +6130,9 @@ StartupXLOG(void)
 	 * commit timestamp.
 	 */
 	CompleteCommitTsInitialization();
+
+	if (wasInRecovery && after_checkpoint_cleanup_hook)
+		after_checkpoint_cleanup_hook(EndOfLog, 0);
 
 	/*
 	 * All done with end-of-recovery actions.
@@ -7318,6 +7329,9 @@ CreateCheckPoint(int flags)
 	if (!RecoveryInProgress())
 		TruncateSUBTRANS(GetOldestTransactionIdConsideredRunning());
 
+	if (after_checkpoint_cleanup_hook)
+		after_checkpoint_cleanup_hook(ProcLastRecPtr, flags);
+
 	/* Real work is done; log and update stats. */
 	LogCheckpointEnd(false);
 
@@ -7477,6 +7491,8 @@ CreateOverwriteContrecordRecord(XLogRecPtr aborted_lsn, XLogRecPtr pagePtr,
 static void
 CheckPointGuts(XLogRecPtr checkPointRedo, int flags)
 {
+	if (CheckPoint_hook)
+		CheckPoint_hook(checkPointRedo, flags);
 	CheckPointRelationMap();
 	CheckPointReplicationSlots(flags & CHECKPOINT_IS_SHUTDOWN);
 	CheckPointSnapBuild();
@@ -9478,3 +9494,5 @@ SetWalWriterSleeping(bool sleeping)
 	XLogCtl->WalWriterSleeping = sleeping;
 	SpinLockRelease(&XLogCtl->info_lck);
 }
+
+void (*RedoShutdownHook) (void) = NULL;
