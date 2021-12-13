bb511ab0098cf7539a17d0c97707c5a6271fe482
Alexander Korotkov <akorotkov@postgresql.org>
2021-12-13 03:17:36 +0300

    Don't cancel recovery processes because of deadlocks

--- src/backend/storage/lmgr/proc.c
+++ src/backend/storage/lmgr/proc.c
@@ -1192,7 +1192,7 @@ ProcSleep(LOCALLOCK *locallock, LockMethod lockMethodTable)
 	 * If InHotStandby we set lock waits slightly later for clarity with other
 	 * code.
 	 */
-	if (!InHotStandby)
+	if (!InHotStandby && !InRecovery)
 	{
 		if (LockTimeout > 0)
 		{
@@ -1552,7 +1552,7 @@ ProcSleep(LOCALLOCK *locallock, LockMethod lockMethodTable)
 	 * already caused QueryCancelPending to become set, we want the cancel to
 	 * be reported as a lock timeout, not a user cancel.
 	 */
-	if (!InHotStandby)
+	if (!InHotStandby && !InRecovery)
 	{
 		if (LockTimeout > 0)
 		{
