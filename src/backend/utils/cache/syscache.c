ae03e4003a31c6c11b8de7de7f22aa5aae64390b
Ilya Kobets <i.kobets@orioledb.com>
2022-08-30 17:37:59 +0300

    Hook fixes for functions call in orioledb recovery
    - ExecInitFuncHook removed
    - Added SearchCatCacheInternal_hook, SearchCatCacheList_hook
    - Added SysCacheGetAttr_hook
    - Fixed type_elements_cmp_hook usage if hook is not set
    - Added ORIOLEDB_PATCHSET_VERSION variable to validate during orioledb build

--- src/backend/utils/cache/syscache.c
+++ src/backend/utils/cache/syscache.c
@@ -696,6 +696,7 @@ static int	SysCacheSupportingRelOidSize;
 
 static int	oid_compare(const void *a, const void *b);
 
+SysCacheGetAttr_hook_type SysCacheGetAttr_hook = NULL;
 
 /*
  * InitCatalogCache - initialize the caches
@@ -1080,6 +1081,7 @@ SysCacheGetAttr(int cacheId, HeapTuple tup,
 				AttrNumber attributeNumber,
 				bool *isNull)
 {
+	TupleDesc cc_tupdesc = SysCache[cacheId]->cc_tupdesc;
 	/*
 	 * We just need to get the TupleDesc out of the cache entry, and then we
 	 * can apply heap_getattr().  Normally the cache control data is already
@@ -1089,14 +1091,18 @@ SysCacheGetAttr(int cacheId, HeapTuple tup,
 	if (cacheId < 0 || cacheId >= SysCacheSize ||
 		!PointerIsValid(SysCache[cacheId]))
 		elog(ERROR, "invalid cache ID: %d", cacheId);
-	if (!PointerIsValid(SysCache[cacheId]->cc_tupdesc))
+
+	if (!PointerIsValid(cc_tupdesc) && SysCacheGetAttr_hook)
+		cc_tupdesc = SysCacheGetAttr_hook(SysCache[cacheId]);
+	if (!PointerIsValid(cc_tupdesc))
 	{
 		InitCatCachePhase2(SysCache[cacheId], false);
 		Assert(PointerIsValid(SysCache[cacheId]->cc_tupdesc));
+		cc_tupdesc = SysCache[cacheId]->cc_tupdesc;
 	}
 
 	return heap_getattr(tup, attributeNumber,
-						SysCache[cacheId]->cc_tupdesc,
+						cc_tupdesc,
 						isNull);
 }
 
