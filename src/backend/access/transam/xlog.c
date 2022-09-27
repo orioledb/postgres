a4c729c9a81706b8d4e2412bbe3b62e689ab4cf3
Michael Paquier <michael@paquier.xyz>
2022-09-27 09:15:07 +0900

    Remove dependency to StringInfo in xlogbackup.{c.h}
    This was used as the returned result type of the generated contents for
    the backup_label and backup history files.  This is replaced by a simple
    string, reducing the cleanup burden of all the callers of
    build_backup_content().
    
    Reviewed-by: Bharath Rupireddy
    Discussion: https://postgr.es/m/YzERvNPaZivHEKZJ@paquier.xyz

--- src/backend/access/transam/xlog.c
+++ src/backend/access/transam/xlog.c
@@ -8587,7 +8587,7 @@ do_pg_backup_stop(BackupState *state, bool waitforarchive)
 	}
 	else
 	{
-		StringInfo	history_file;
+		char	   *history_file;
 
 		/*
 		 * Write the backup-end xlog record
@@ -8627,8 +8627,7 @@ do_pg_backup_stop(BackupState *state, bool waitforarchive)
 
 		/* Build and save the contents of the backup history file */
 		history_file = build_backup_content(state, true);
-		fprintf(fp, "%s", history_file->data);
-		pfree(history_file->data);
+		fprintf(fp, "%s", history_file);
 		pfree(history_file);
 
 		if (fflush(fp) || ferror(fp) || FreeFile(fp))
