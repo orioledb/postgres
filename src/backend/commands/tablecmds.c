beb6757d11b723382b72fc890c8847639721fa7d
Ilya Kobets <i.kobets@orioledb.com>
2022-11-01 19:28:23 +0300

    - Added additional tableam functions - Added ALTER not null hooks added - Added alter_column_type hook, define_index hook

--- src/backend/commands/tablecmds.c
+++ src/backend/commands/tablecmds.c
@@ -5501,7 +5501,7 @@ ATRewriteTables(AlterTableStmt *parsetree, List **wqueue, LOCKMODE lockmode,
 			 * unlogged anyway.
 			 */
 			if (!table_has_extended_am(OldHeap) ||
-				!table_extended_rewrite_table(OldHeap))
+				!table_extended_rewrite_table(tab->oldDesc, OldHeap))
 			{
 				OIDNewHeap = make_new_heap(tab->relid, NewTableSpace,
 										   persistence, lockmode);
@@ -7181,6 +7181,9 @@ ATExecDropNotNull(Relation rel, const char *colName, LOCKMODE lockmode)
 
 	table_close(attr_rel, RowExclusiveLock);
 
+	if (table_has_extended_am(rel))
+		table_extended_change_not_null(rel, colName, false);
+
 	return address;
 }
 
@@ -7320,6 +7323,9 @@ ATExecSetNotNull(AlteredTableInfo *tab, Relation rel,
 							  RelationGetRelid(rel), attnum);
 
 	table_close(attr_rel, RowExclusiveLock);
+
+	if (table_has_extended_am(rel))
+		table_extended_change_not_null(rel, colName, true);
 
 	return address;
 }
@@ -12011,6 +12017,11 @@ ATExecAlterColumnType(AlteredTableInfo *tab, Relation rel,
 	SysScanDesc scan;
 	HeapTuple	depTup;
 	ObjectAddress address;
+	bool		rebuild_constraint = true;
+
+	if (table_has_extended_am(rel))
+		rebuild_constraint = table_extended_alter_column_type(rel, colName,
+															  def);
 
 	/*
 	 * Clear all the missing values if we're rewriting the table, since this
@@ -12181,7 +12192,8 @@ ATExecAlterColumnType(AlteredTableInfo *tab, Relation rel,
 
 			case OCLASS_CONSTRAINT:
 				Assert(foundObject.objectSubId == 0);
-				RememberConstraintForRebuilding(foundObject.objectId, tab);
+				if (rebuild_constraint)
+					RememberConstraintForRebuilding(foundObject.objectId, tab);
 				break;
 
 			case OCLASS_REWRITE:
