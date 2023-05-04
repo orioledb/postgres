231ef604e7005348c83c6291562f363cf07b4763
Alexander Korotkov <akorotkov@postgresql.org>
2023-05-04 19:22:25 +0300

    have_backup_in_progress() function

--- src/include/access/xlog.h
+++ src/include/access/xlog.h
@@ -286,6 +286,7 @@ extern XLogRecPtr do_pg_backup_start(const char *backupidstr, bool fast,
 extern XLogRecPtr do_pg_backup_stop(char *labelfile, bool waitforarchive,
 									TimeLineID *stoptli_p);
 extern void do_pg_abort_backup(int code, Datum arg);
+extern bool have_backup_in_progress(void);
 extern void register_persistent_abort_backup_handler(void);
 extern SessionBackupState get_backup_status(void);
 
