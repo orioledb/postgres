fcf0426a82432923ef375ce6e87f44f2d71b3e1d
Ilya Kobets <i.kobets@orioledb.com>
2023-05-12 19:36:01 +0300

    Removed redundant hooks, new universal added

--- src/backend/commands/indexcmds.c
+++ src/backend/commands/indexcmds.c
@@ -66,6 +66,7 @@
 #include "utils/snapmgr.h"
 #include "utils/syscache.h"
 
+GetDefaultOpClass_hook_type GetDefaultOpClass_hook = NULL;
 
 /* non-export function prototypes */
 static void CheckPredicate(Expr *predicate);
@@ -2188,6 +2189,9 @@ GetDefaultOpClass(Oid type_id, Oid am_id)
 
 	/* If it's a domain, look at the base type instead */
 	type_id = getBaseType(type_id);
+
+	if (GetDefaultOpClass_hook)
+		return GetDefaultOpClass_hook(type_id, am_id);
 
 	tcategory = TypeCategory(type_id);
 
