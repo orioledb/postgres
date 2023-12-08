6cfb7e69e05991863f7d029d474e8b2c34c82e69
Ilya Kobets <i.kobets@orioledb.com>
2023-12-08 01:37:02 +0100

    Added option to pg_rewind to perform extension specific rewind
    - added option --extension for pg_rewind
    - extracted SimpleXLogRead from extractPageMap for generic wal iteration in pg_rewind

--- src/bin/pg_rewind/pg_rewind.h
+++ src/bin/pg_rewind/pg_rewind.h
@@ -14,6 +14,8 @@
 #include "access/timeline.h"
 #include "common/logging.h"
 #include "common/file_utils.h"
+#include "fe_utils/simple_list.h"
+#include "pg_rewind_ext.h"
 
 /* Configuration options */
 extern char *datadir_target;
@@ -50,5 +52,13 @@ extern void progress_report(bool finished);
 extern TimeLineHistoryEntry *rewind_parseTimeLineHistory(char *buffer,
 														 TimeLineID targetTLI,
 														 int *nentries);
+
+/* in extension.c */
+extern void process_extensions(SimpleStringList *extensions,
+							   const char *datadir_target, char *datadir_source,
+							   char *connstr_source, XLogRecPtr startpoint,
+							   int tliIndex, XLogRecPtr endpoint,
+							   const char *restoreCommand, const char *argv0,
+							   bool debug);
 
 #endif							/* PG_REWIND_H */
