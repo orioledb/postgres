5a64bf0e8001f4ecf734571ef368c6dd10376bf9
Ilya Kobets <i.kobets@orioledb.com>
2024-05-04 04:09:52 +0200

    Index only scan with rowid

--- src/include/access/genam.h
+++ src/include/access/genam.h
@@ -176,6 +176,8 @@ extern IndexScanDesc index_beginscan_parallel(Relation heaprel,
 											  ParallelIndexScanDesc pscan);
 extern ItemPointer index_getnext_tid(IndexScanDesc scan,
 									 ScanDirection direction);
+extern NullableDatum index_getnext_rowid(IndexScanDesc scan,
+										 ScanDirection direction);
 extern Datum index_getnext_tupleid(IndexScanDesc scan, ScanDirection direction);
 struct TupleTableSlot;
 extern bool index_fetch_heap(IndexScanDesc scan, struct TupleTableSlot *slot);
