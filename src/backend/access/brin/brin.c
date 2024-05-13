52bf2f21a96410805f5a93aa11a24dba69cc3811
Ilya Kobets <i.kobets@orioledb.com>
2024-05-13 20:33:54 +0200

    Passing tupleid to insert now

--- src/backend/access/brin/brin.c
+++ src/backend/access/brin/brin.c
@@ -342,7 +342,7 @@ initialize_brin_insertstate(Relation idxRel, IndexInfo *indexInfo)
  */
 bool
 brininsert(Relation idxRel, Datum *values, bool *nulls,
-		   ItemPointer heaptid, Relation heapRel,
+		   Datum tupleid, Relation heapRel,
 		   IndexUniqueCheck checkUnique,
 		   bool indexUnchanged,
 		   IndexInfo *indexInfo)
@@ -357,6 +357,7 @@ brininsert(Relation idxRel, Datum *values, bool *nulls,
 	MemoryContext tupcxt = NULL;
 	MemoryContext oldcxt = CurrentMemoryContext;
 	bool		autosummarize = BrinGetAutoSummarize(idxRel);
+	ItemPointer heaptid = DatumGetItemPointer(tupleid);
 
 	/*
 	 * If first time through in this statement, initialize the insert state
