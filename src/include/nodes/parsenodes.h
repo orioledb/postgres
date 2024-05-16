d78873e15187e650200898700e2d1ed5d424a7ec
Ilya Kobets <i.kobets@orioledb.com>
2024-05-17 00:27:02 +0200

    Methods for index update and delete
    Also validates compatability of index AM with table AM at index creation

--- src/include/nodes/parsenodes.h
+++ src/include/nodes/parsenodes.h
@@ -2824,6 +2824,7 @@ typedef struct CreateAmStmt
 	char	   *amname;			/* access method name */
 	List	   *handler_name;	/* handler function name */
 	char		amtype;			/* type of access method */
+	char	   *tableam_name;	/* table AM name */
 } CreateAmStmt;
 
 /* ----------------------
