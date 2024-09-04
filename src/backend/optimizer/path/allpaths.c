e518d1bdbfc449d30ea01d76bf4cea865077e79c
Pavel Borisov <pashkin.elfe@gmail.com>
2024-09-04 18:02:31 +0400

    to table am (2)

--- src/backend/optimizer/path/allpaths.c
+++ src/backend/optimizer/path/allpaths.c
@@ -773,8 +773,6 @@ set_plain_rel_pathlist(PlannerInfo *root, RelOptInfo *rel, RangeTblEntry *rte)
 	 */
 	required_outer = rel->lateral_relids;
 
-	/* Consider sequential scan */
-	add_path(rel, create_seqscan_path(root, rel, required_outer, 0));
 	if (!set_plain_rel_pathlist_hook ||
 		set_plain_rel_pathlist_hook(root, rel, rte))
 	{
