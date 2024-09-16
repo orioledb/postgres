12501ccea103b651d342c3594c326b3adf5e7833
Alexander Korotkov <akorotkov@postgresql.org>
2024-09-17 01:58:24 +0300

    Fix handling tupleid in logical replication

--- src/backend/replication/logical/worker.c
+++ src/backend/replication/logical/worker.c
@@ -2444,9 +2444,8 @@ apply_handle_insert(StringInfo s)
 	/* Initialize the executor state. */
 	edata = create_edata_for_relation(rel);
 	estate = edata->estate;
-	remoteslot = ExecInitExtraTupleSlot(estate,
-										RelationGetDescr(rel->localrel),
-										&TTSOpsVirtual);
+	remoteslot = table_slot_create(rel->localrel,
+								   &estate->es_tupleTable);
 
 	/* Process and store remote tuple in the slot */
 	oldctx = MemoryContextSwitchTo(GetPerTupleMemoryContext(estate));
@@ -2608,9 +2607,8 @@ apply_handle_update(StringInfo s)
 	/* Initialize the executor state. */
 	edata = create_edata_for_relation(rel);
 	estate = edata->estate;
-	remoteslot = ExecInitExtraTupleSlot(estate,
-										RelationGetDescr(rel->localrel),
-										&TTSOpsVirtual);
+	remoteslot = table_slot_create(rel->localrel,
+								   &estate->es_tupleTable);
 
 	/*
 	 * Populate updatedCols so that per-column triggers can fire, and so
@@ -2817,9 +2815,8 @@ apply_handle_delete(StringInfo s)
 	/* Initialize the executor state. */
 	edata = create_edata_for_relation(rel);
 	estate = edata->estate;
-	remoteslot = ExecInitExtraTupleSlot(estate,
-										RelationGetDescr(rel->localrel),
-										&TTSOpsVirtual);
+	remoteslot = table_slot_create(rel->localrel,
+								   &estate->es_tupleTable);
 
 	/* Build the search tuple. */
 	oldctx = MemoryContextSwitchTo(GetPerTupleMemoryContext(estate));
