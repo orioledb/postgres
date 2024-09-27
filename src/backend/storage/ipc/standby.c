785f2df4fb9da045baf85db534703d7a726170c8
Ilya Kobets <i.kobets@orioledb.com>
2024-09-27 14:26:40 +0200

    New CSN snapshot format
     * Add xlogptr and xmin to determine right order of transactions when
       decoding on replica.
     * Add CSN snapshot data to snapshot builder.
     * Record CSN to the running xids and restore it during logical decoding
       to the snapshot builder.
     * Add function to update CSN snapshot data in snapshot builder.
     * Update CSN snapshot LSN in snapshot building after each transaction commit.
     * Restore CSN snapshot data in SnapBuildBuildSnapshot().

--- src/backend/storage/ipc/standby.c
+++ src/backend/storage/ipc/standby.c
@@ -1361,6 +1361,7 @@ LogCurrentRunningXacts(RunningTransactions CurrRunningXacts)
 	xlrec.nextXid = CurrRunningXacts->nextXid;
 	xlrec.oldestRunningXid = CurrRunningXacts->oldestRunningXid;
 	xlrec.latestCompletedXid = CurrRunningXacts->latestCompletedXid;
+	xlrec.csn = CurrRunningXacts->csn;
 
 	/* Header */
 	XLogBeginInsert();
