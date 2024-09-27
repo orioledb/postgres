7f152e12a4c6eb7dbe88de8db3232c67f2568cc2
Ilya Kobets <i.kobets@orioledb.com>
2024-09-27 14:26:40 +0200

    New CSN snapshot format
     * Add xlogptr and xmin to determine right order of transactions when
       decoding on replica.
     * Add CSN snapshot data to snapshot builder.
     * Record CSN to the running xids and restore it during logical decoding
       to the snapshot builder.
     * Add function to update CSN snapshot data in snapshot builder.
     * Update CSN snapshot LSN in snapshot building after each transaction commit.
     * Restore CSN snapshot data in SnapBuildBuildSnapshot().

--- src/backend/replication/logical/snapbuild.c
+++ src/backend/replication/logical/snapbuild.c
@@ -219,6 +219,8 @@ struct SnapBuild
 	 */
 	TransactionId next_phase_at;
 
+	CSNSnapshotData csnSnapshotData;
+
 	/*
 	 * Array of transactions which could have catalog changes that committed
 	 * between xmin and xmax.
@@ -575,6 +577,8 @@ SnapBuildBuildSnapshot(SnapBuild *builder)
 	snapshot->active_count = 0;
 	snapshot->regd_count = 0;
 	snapshot->snapXactCompletionCount = 0;
+
+	snapshot->csnSnapshotData = builder->csnSnapshotData;
 
 	return snapshot;
 }
@@ -1053,6 +1057,8 @@ SnapBuildCommitTxn(SnapBuild *builder, XLogRecPtr lsn, TransactionId xid,
 
 	TransactionId xmax = xid;
 
+	builder->csnSnapshotData.xlogptr = lsn;
+
 	/*
 	 * Transactions preceding BUILDING_SNAPSHOT will neither be decoded, nor
 	 * will they be part of a snapshot.  So we don't need to record anything.
@@ -1267,6 +1273,9 @@ SnapBuildProcessRunningXacts(SnapBuild *builder, XLogRecPtr lsn, xl_running_xact
 	 * we hit fast paths in heapam_visibility.c.
 	 */
 	builder->xmin = running->oldestRunningXid;
+	builder->csnSnapshotData.snapshotcsn = running->csn;
+	builder->csnSnapshotData.xmin = 0;
+	builder->csnSnapshotData.xlogptr = lsn;
 
 	/* Remove transactions we don't need to keep track off anymore */
 	SnapBuildPurgeOlderTxn(builder);
@@ -2184,4 +2193,11 @@ SnapBuildSnapshotExists(XLogRecPtr lsn)
 				 errmsg("could not stat file \"%s\": %m", path)));
 
 	return ret == 0;
+}
+
+void
+SnapBuildUpdateCSNSnaphot(SnapBuild *builder,
+						  CSNSnapshotData *csnSnapshotData)
+{
+	builder->csnSnapshotData = *csnSnapshotData;
 }
