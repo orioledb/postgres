39a44d233b29116d422b9e4013ea0712e27b213c
Alexander Korotkov <akorotkov@postgresql.org>
2024-10-08 21:31:33 +0300

    Add handling of CSN snapshot in some places of snapbuild.c

--- src/backend/replication/logical/snapbuild.c
+++ src/backend/replication/logical/snapbuild.c
@@ -663,6 +663,7 @@ SnapBuildInitialSnapshot(SnapBuild *builder)
 	snap->snapshot_type = SNAPSHOT_MVCC;
 	snap->xcnt = newxcnt;
 	snap->xip = newxip;
+	snap->csnSnapshotData = builder->csnSnapshotData;
 
 	return snap;
 }
@@ -1271,6 +1272,10 @@ SnapBuildProcessRunningXacts(SnapBuild *builder, XLogRecPtr lsn, xl_running_xact
 	ReorderBufferTXN *txn;
 	TransactionId xmin;
 
+	builder->csnSnapshotData.snapshotcsn = running->csn;
+	builder->csnSnapshotData.xmin = 0;
+	builder->csnSnapshotData.xlogptr = lsn;
+
 	/*
 	 * If we're not consistent yet, inspect the record to see whether it
 	 * allows to get closer to being consistent. If we are consistent, dump
@@ -1298,9 +1303,6 @@ SnapBuildProcessRunningXacts(SnapBuild *builder, XLogRecPtr lsn, xl_running_xact
 	 * we hit fast paths in heapam_visibility.c.
 	 */
 	builder->xmin = running->oldestRunningXid;
-	builder->csnSnapshotData.snapshotcsn = running->csn;
-	builder->csnSnapshotData.xmin = 0;
-	builder->csnSnapshotData.xlogptr = lsn;
 
 	/* Remove transactions we don't need to keep track off anymore */
 	SnapBuildPurgeOlderTxn(builder);
