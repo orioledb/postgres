2a5350893492de202704baeb10118b042ea98af2
Alexander Korotkov <akorotkov@postgresql.org>
2024-10-14 16:22:14 +0300

    Move CheckPoint_hook() call after CheckPointBuffers()
    That allows to process flushed buffers in CheckPoint_hook().

--- src/backend/access/transam/xlog.c
+++ src/backend/access/transam/xlog.c
@@ -7117,8 +7117,6 @@ CreateOverwriteContrecordRecord(XLogRecPtr aborted_lsn, XLogRecPtr pagePtr,
 static void
 CheckPointGuts(XLogRecPtr checkPointRedo, int flags)
 {
-	if (CheckPoint_hook)
-		CheckPoint_hook(checkPointRedo, flags);
 	CheckPointRelationMap();
 	CheckPointReplicationSlots();
 	CheckPointSnapBuild();
@@ -7134,6 +7132,9 @@ CheckPointGuts(XLogRecPtr checkPointRedo, int flags)
 	CheckPointMultiXact();
 	CheckPointPredicate();
 	CheckPointBuffers(flags);
+
+	if (CheckPoint_hook)
+		CheckPoint_hook(checkPointRedo, flags);
 
 	/* Perform all queued up fsyncs */
 	TRACE_POSTGRESQL_BUFFER_CHECKPOINT_SYNC_START();
