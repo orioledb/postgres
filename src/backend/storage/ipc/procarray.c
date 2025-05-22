ce4015bc012677663acebd5c3362dffa5fbfbce7
Pavel Borisov <pashkin.elfe@gmail.com>
2025-05-22 17:19:03 +0400

    Modify CountOtherDBBackends/TerminateOtherDBBackends to process whole cluster by providing invalid databaseId

--- src/backend/storage/ipc/procarray.c
+++ src/backend/storage/ipc/procarray.c
@@ -3753,6 +3753,8 @@ CountUserBackends(Oid roleid)
  * backend startup.  The caller should normally hold an exclusive lock on the
  * target DB before calling this, which is one reason we mustn't wait
  * indefinitely.
+ *
+ * If databaseId is InvalidOid, count all backends in a cluster.
  */
 bool
 CountOtherDBBackends(Oid databaseId, int *nbackends, int *nprepared)
@@ -3782,7 +3784,7 @@ CountOtherDBBackends(Oid databaseId, int *nbackends, int *nprepared)
 			PGPROC	   *proc = &allProcs[pgprocno];
 			uint8		statusFlags = ProcGlobal->statusFlags[index];
 
-			if (proc->databaseId != databaseId)
+			if (databaseId != InvalidOid && proc->databaseId != databaseId)
 				continue;
 			if (proc == MyProc)
 				continue;
@@ -3831,6 +3833,8 @@ CountOtherDBBackends(Oid databaseId, int *nbackends, int *nprepared)
  *
  * If the target database has a prepared transaction or permissions checks
  * fail for a connection, this fails without terminating anything.
+ *
+ * If databaseId is InvalidOid, terminate all backends in a cluster.
  */
 void
 TerminateOtherDBBackends(Oid databaseId)
@@ -3847,7 +3851,7 @@ TerminateOtherDBBackends(Oid databaseId)
 		int			pgprocno = arrayP->pgprocnos[i];
 		PGPROC	   *proc = &allProcs[pgprocno];
 
-		if (proc->databaseId != databaseId)
+		if (databaseId != InvalidOid && proc->databaseId != databaseId)
 			continue;
 		if (proc == MyProc)
 			continue;
@@ -3861,7 +3865,9 @@ TerminateOtherDBBackends(Oid databaseId)
 	LWLockRelease(ProcArrayLock);
 
 	if (nprepared > 0)
-		ereport(ERROR,
+	{
+		if (databaseId != InvalidOid)
+			ereport(ERROR,
 				(errcode(ERRCODE_OBJECT_IN_USE),
 				 errmsg("database \"%s\" is being used by prepared transactions",
 						get_database_name(databaseId)),
@@ -3869,6 +3875,15 @@ TerminateOtherDBBackends(Oid databaseId)
 								  "There are %d prepared transactions using the database.",
 								  nprepared,
 								  nprepared)));
+		else
+			ereport(ERROR,
+				(errcode(ERRCODE_OBJECT_IN_USE),
+				 errmsg("cluster is being used by prepared transactions"),
+				 errdetail_plural("There is %d prepared transaction using the cluster.",
+								  "There are %d prepared transactions using the cluster.",
+								  nprepared,
+								  nprepared)));
+	}
 
 	if (pids)
 	{
