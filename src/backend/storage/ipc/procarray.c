182db0b009ae2361c23ae0debbd81602433c07d5
Pavel Borisov <pashkin.elfe@gmail.com>
2025-05-26 20:12:27 +0400

    Make CountOtherDBBackends to skip background workers
    NB: Autovacuum launcher is not a bgworker and it's counted

--- src/backend/storage/ipc/procarray.c
+++ src/backend/storage/ipc/procarray.c
@@ -3754,7 +3754,7 @@ CountUserBackends(Oid roleid)
  * target DB before calling this, which is one reason we mustn't wait
  * indefinitely.
  *
- * If databaseId is InvalidOid, count all backends in a cluster.
+ * If databaseId is InvalidOid, count all non-bgworker backends in a cluster.
  */
 bool
 CountOtherDBBackends(Oid databaseId, int *nbackends, int *nprepared)
@@ -3784,8 +3784,17 @@ CountOtherDBBackends(Oid databaseId, int *nbackends, int *nprepared)
 			PGPROC	   *proc = &allProcs[pgprocno];
 			uint8		statusFlags = ProcGlobal->statusFlags[index];
 
-			if (databaseId != InvalidOid && proc->databaseId != databaseId)
-				continue;
+			if (databaseId != InvalidOid)
+			{
+				if (proc->databaseId != databaseId)
+					continue;
+			}
+			else
+			{
+				if (proc->isBackgroundWorker)
+					continue;	/* do not count background workers */
+			}
+
 			if (proc == MyProc)
 				continue;
 
