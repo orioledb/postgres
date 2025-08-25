ebe764a3f960a8a2c11981df387389fc12b62334
Ilya Kobets <i.kobets@orioledb.com>
2025-08-25 21:03:04 +0200

    Added support for bytea* tupleid for exclusion constraints

--- src/include/access/genam.h
+++ src/include/access/genam.h
@@ -144,7 +144,7 @@ extern void index_close(Relation relation, LOCKMODE lockmode);
 
 extern bool index_insert(Relation indexRelation,
 						 Datum *values, bool *isnull,
-						 ItemPointer tupleid,
+						 Datum tupleidDatum,
 						 Relation heapRelation,
 						 IndexUniqueCheck checkUnique,
 						 bool indexUnchanged,
