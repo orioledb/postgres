bc16f9519e22033ccac85dc151aa21a444911fb0
Artur Zakirov <zaartur@gmail.com>
2025-08-26 14:58:42 +0200

    Issue #487: Add TableSupportsBackwardScan() and TableAmRoutine->amcanbackward
    OrioleDB table AM doesn't support BACKWARD SCAN in all cases. A user
    needs to declare a cursor with SCROLL option.

--- src/include/access/tableam.h
+++ src/include/access/tableam.h
@@ -304,6 +304,8 @@ typedef struct TableAmRoutine
 	/* this must be set to T_TableAmRoutine */
 	NodeTag		type;
 
+	/* does AM support backward scanning? */
+	bool		amcanbackward;
 
 	/* ------------------------------------------------------------------------
 	 * Slot related callbacks.
