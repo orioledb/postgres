6930465b4976d38c0d5c2715a72fe534e23043cf
Alexander Korotkov <akorotkov@postgresql.org>
2025-09-24 01:41:41 +0300

    Fix copying datum in ExecModifyTable()
    Save the alignment.

--- src/backend/executor/nodeModifyTable.c
+++ src/backend/executor/nodeModifyTable.c
@@ -3714,7 +3714,9 @@ ExecModifyTable(PlanState *pstate)
 					if (isNull)
 						elog(ERROR, "rowid is NULL");
 
-					tupleid = datumCopy(datum, false, -1);
+					tupleid = PointerGetDatum(PG_DETOAST_DATUM(datum));
+					if (tupleid == datum)
+						tupleid = datumCopy(datum, false, -1);
 				}
 			}
 
