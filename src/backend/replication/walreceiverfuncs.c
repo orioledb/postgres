a54a771982be424807ac0b427848dd288349a010
Alexander Korotkov <akorotkov@postgresql.org>
2025-09-30 14:00:37 +0300

    Add GetReplayXlogPtrHook allowing extensions to adjust replay LSN
    Useful when extension performs some async operations in the WAL record
    redo function.

--- src/backend/replication/walreceiverfuncs.c
+++ src/backend/replication/walreceiverfuncs.c
@@ -373,7 +373,7 @@ GetReplicationApplyDelay(void)
 	receivePtr = walrcv->flushedUpto;
 	SpinLockRelease(&walrcv->mutex);
 
-	replayPtr = GetXLogReplayRecPtr(NULL);
+	replayPtr = GetEffectiveXlogReplayRecPtr();
 
 	if (receivePtr == replayPtr)
 		return 0;
