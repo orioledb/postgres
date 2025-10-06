28faab52ec0a4a9b417734b670ff14468fbb00ec
Egor Ivkov <e.o.ivkov@gmail.com>
2025-10-06 18:17:04 +0300

    TableAM: add relation_reindex

--- src/backend/commands/tablecmds.c
+++ src/backend/commands/tablecmds.c
@@ -2145,7 +2145,6 @@ ExecuteTruncateGuts(List *explicit_rels,
 		}
 		else
 		{
-			Oid			heap_relid;
 			Oid			toast_relid;
 			ReindexParams reindex_params = {0};
 
@@ -2166,8 +2165,6 @@ ExecuteTruncateGuts(List *explicit_rels,
 			 */
 			RelationSetNewRelfilenumber(rel, rel->rd_rel->relpersistence);
 
-			heap_relid = RelationGetRelid(rel);
-
 			/*
 			 * The same for the toast table, if any.
 			 */
@@ -2185,8 +2182,8 @@ ExecuteTruncateGuts(List *explicit_rels,
 			/*
 			 * Reconstruct the indexes to match, and we're done.
 			 */
-			reindex_relation(NULL, heap_relid, REINDEX_REL_PROCESS_TOAST,
-							 &reindex_params);
+			table_relation_reindex(rel, NULL, REINDEX_REL_PROCESS_TOAST,
+										 &reindex_params);
 		}
 
 		pgstat_count_truncate(rel);
