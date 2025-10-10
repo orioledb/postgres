1b6365bc8eeba0fa155fd39604a633207137fe22
Egor Ivkov <e.o.ivkov@gmail.com>
2025-10-10 20:11:29 +0300

    IndexAM: Add amreuse

--- src/backend/access/spgist/spgutils.c
+++ src/backend/access/spgist/spgutils.c
@@ -69,6 +69,7 @@ spghandler(PG_FUNCTION_ARGS)
 	amroutine->amkeytype = InvalidOid;
 
 	amroutine->ambuild = spgbuild;
+	amroutine->amreuse = NULL;
 	amroutine->ambuildempty = spgbuildempty;
 	amroutine->aminsert = NULL;
 	amroutine->aminsertextended = spginsert;
