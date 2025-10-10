15fcb04502b4776aaa0d894bdbaf353597ecb123
Egor Ivkov <e.o.ivkov@gmail.com>
2025-10-10 20:11:29 +0300

    IndexAM: Add amreuse

--- contrib/bloom/blutils.c
+++ contrib/bloom/blutils.c
@@ -130,6 +130,7 @@ blhandler(PG_FUNCTION_ARGS)
 	amroutine->amkeytype = InvalidOid;
 
 	amroutine->ambuild = blbuild;
+	amroutine->amreuse = NULL;
 	amroutine->ambuildempty = blbuildempty;
 	amroutine->aminsert = NULL;
 	amroutine->aminsertextended = blinsert;
