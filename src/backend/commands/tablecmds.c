c2e128e7d0513c123ad1a57beb9ac15f27c4a37d
Ilya Kobets <i.kobets@orioledb.com>
2025-10-22 18:23:43 +0200

    Revert "Merge pull request #25 from e-ivkov/truncate-reindex"
    This reverts commit d7ccd3d4c403626ed16409ed7e5eea9f71bfde64, reversing
    changes made to 7698e716b7d3c038c08426b0506ae50f7a040afe.

--- src/backend/commands/tablecmds.c
+++ src/backend/commands/tablecmds.c
@@ -2216,6 +2216,7 @@ ExecuteTruncateGuts(List *explicit_rels,
 		}
 		else
 		{
+			Oid			heap_relid;
 			Oid			toast_relid;
 			ReindexParams reindex_params = {0};
 
@@ -2236,6 +2237,8 @@ ExecuteTruncateGuts(List *explicit_rels,
 			 */
 			RelationSetNewRelfilenumber(rel, rel->rd_rel->relpersistence);
 
+			heap_relid = RelationGetRelid(rel);
+
 			/*
 			 * The same for the toast table, if any.
 			 */
@@ -2253,8 +2256,8 @@ ExecuteTruncateGuts(List *explicit_rels,
 			/*
 			 * Reconstruct the indexes to match, and we're done.
 			 */
-			table_relation_reindex(rel, NULL, REINDEX_REL_PROCESS_TOAST,
-										 &reindex_params);
+			reindex_relation(NULL, heap_relid, REINDEX_REL_PROCESS_TOAST,
+							 &reindex_params);
 		}
 
 		pgstat_count_truncate(rel);
