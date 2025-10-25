161379e4885aba702a24e98e368a95171483cd21
Alexander Korotkov <akorotkov@postgresql.org>
2025-10-25 21:04:34 +0300

    Allow skipping transactions for ReorderBufferAbortOld()
    Transactions need to be marked as RBTXN_DISTR_SKIP_CLEANUP to be skipped in
    ReorderBufferAbortOld().

--- src/backend/replication/logical/reorderbuffer.c
+++ src/backend/replication/logical/reorderbuffer.c
@@ -2992,7 +2992,8 @@ ReorderBufferAbortOld(ReorderBuffer *rb, TransactionId oldestRunningXid)
 
 		txn = dlist_container(ReorderBufferTXN, node, it.cur);
 
-		if (TransactionIdPrecedes(txn->xid, oldestRunningXid))
+		if (!(txn->txn_flags & RBTXN_DISTR_SKIP_CLEANUP) &&
+			TransactionIdPrecedes(txn->xid, oldestRunningXid))
 		{
 			elog(DEBUG2, "aborting old transaction %u", txn->xid);
 
