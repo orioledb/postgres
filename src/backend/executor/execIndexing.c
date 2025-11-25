742f6eb26ac925762c21bf1cf6d18e18d8f1fee9
Ilya Kobets <i.kobets@orioledb.com>
2025-11-25 03:59:16 +0100

    Pass indexUnchanged to index_update method

--- src/backend/executor/execIndexing.c
+++ src/backend/executor/execIndexing.c
@@ -581,6 +581,7 @@ ExecUpdateIndexTuples(ResultRelInfo *resultRelInfo,
 		IndexInfo  *indexInfo;
 		bool		applyNoDupErr;
 		IndexUniqueCheck checkUnique;
+		bool		indexUnchanged;
 		bool		satisfiesConstraint;
 		bool		new_valid = true;
 
@@ -672,6 +673,16 @@ ExecUpdateIndexTuples(ResultRelInfo *resultRelInfo,
 		else
 			checkUnique = UNIQUE_CHECK_PARTIAL;
 
+		/*
+		* There's definitely going to be an index_insert() call for this
+		* index.  If we're being called as part of an UPDATE statement,
+		* consider if the 'indexUnchanged' = true hint should be passed.
+		*/
+		indexUnchanged = index_unchanged_by_update(resultRelInfo,
+												   estate,
+												   indexInfo,
+												   indexRelation);
+
 		if (indexRelation->rd_indam->ammvccaware)
 		{
 			Datum		valuesOld[INDEX_MAX_KEYS];
@@ -733,22 +744,12 @@ ExecUpdateIndexTuples(ResultRelInfo *resultRelInfo,
 							 oldTupleidDatum,
 							 heapRelation,	/* heap relation */
 							 checkUnique,	/* type of uniqueness check to do */
+							 indexUnchanged,	/* UPDATE without logical change? */
 							 indexInfo);	/* index AM may need this */
 
 		}
 		else
 		{
-			bool		indexUnchanged;
-			/*
-			* There's definitely going to be an index_insert() call for this
-			* index.  If we're being called as part of an UPDATE statement,
-			* consider if the 'indexUnchanged' = true hint should be passed.
-			*/
-			indexUnchanged = index_unchanged_by_update(resultRelInfo,
-													   estate,
-													   indexInfo,
-													   indexRelation);
-
 			satisfiesConstraint =
 				index_insert(indexRelation, /* index relation */
 							 values,	/* array of index Datums */
@@ -1274,9 +1275,10 @@ retry:
 				continue;
 			}
 		} else {
-			Assert(tupleidDatum > 0);
-			Pointer rowid = DatumGetPointer(tupleidDatum);
+			Pointer rowid;
 
+			Assert(tupleidDatum > 0);
+			rowid = DatumGetPointer(tupleidDatum);
 			if (PointerIsValid(rowid))
 			{
 				bool	isnull;
--- src/include/access/genam.h
+++ src/include/access/genam.h
@@ -185,6 +185,7 @@ extern bool index_update(Relation indexRelation,
 						 Datum oldTupleid,
 						 Relation heapRelation,
 						 IndexUniqueCheck checkUnique,
+						 bool indexUnchanged,
 						 struct IndexInfo *indexInfo);
 extern bool index_delete(Relation indexRelation, Datum *values, bool *isnull,
 						 Datum tupleid, Relation heapRelation,
