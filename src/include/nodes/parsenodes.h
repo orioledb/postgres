94a5a4ff2cc1f4062d988094c1da74ad9edf8672
Елизавета Реутт <eareutt@sberbank.ru>
2025-11-27 15:35:58 +0300

    Ignore RowRefType field in RangeTblEntry on jumble query
    Added RowRefType field in RangeTblEntry should not change queryid
    calculated in jumble query. It is internal field which is not depend
    on query parsing.

--- src/include/nodes/parsenodes.h
+++ src/include/nodes/parsenodes.h
@@ -1103,7 +1103,7 @@ typedef struct RangeTblEntry
 	Index		perminfoindex pg_node_attr(query_jumble_ignore);
 	/* sampling info, or NULL */
 	struct TableSampleClause *tablesample;
-	RowRefType	reftype;
+	RowRefType	reftype pg_node_attr(query_jumble_ignore);
 
 	/*
 	 * Fields valid for a subquery RTE (else NULL):
