6b94bcfee9ae88aaa5afdd9c68eb910419fe8d8b
Ilya Kobets <i.kobets@orioledb.com>
2025-12-04 04:47:37 +0100

    pass indexRel to table_index_fetch_begin to detect index bridging for orioledb

--- src/backend/access/table/tableam.c
+++ src/backend/access/table/tableam.c
@@ -216,6 +216,7 @@ table_beginscan_parallel(Relation relation, ParallelTableScanDesc pscan)
  */
 bool
 table_index_fetch_tuple_check(Relation rel,
+							  Relation indexRel,
 							  ItemPointer tid,
 							  Snapshot snapshot,
 							  bool *all_dead)
@@ -226,7 +227,7 @@ table_index_fetch_tuple_check(Relation rel,
 	bool		found;
 
 	slot = table_slot_create(rel, NULL);
-	scan = table_index_fetch_begin(rel);
+	scan = table_index_fetch_begin(rel, indexRel);
 	found = table_index_fetch_tuple(scan, PointerGetDatum(tid), snapshot, slot, &call_again,
 									all_dead);
 	table_index_fetch_end(scan);
