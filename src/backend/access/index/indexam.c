880c80cc78dbfd07516be77f1714f86810c61830
Ilya Kobets <i.kobets@orioledb.com>
2025-12-04 04:47:37 +0100

    pass indexRel to table_index_fetch_begin to detect index bridging for orioledb

--- src/backend/access/index/indexam.c
+++ src/backend/access/index/indexam.c
@@ -340,7 +340,7 @@ index_beginscan(Relation heapRelation,
 	scan->xs_snapshot = snapshot;
 
 	/* prepare to fetch index matches from table */
-	scan->xs_heapfetch = table_index_fetch_begin(heapRelation);
+	scan->xs_heapfetch = table_index_fetch_begin(heapRelation, indexRelation);
 
 	return scan;
 }
@@ -628,7 +628,7 @@ index_beginscan_parallel(Relation heaprel, Relation indexrel, int nkeys,
 	scan->xs_snapshot = snapshot;
 
 	/* prepare to fetch index matches from table */
-	scan->xs_heapfetch = table_index_fetch_begin(heaprel);
+	scan->xs_heapfetch = table_index_fetch_begin(heaprel, indexrel);
 
 	return scan;
 }
