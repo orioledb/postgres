3e6f12371329a7343af647a7fd239f69a92ea2c7
Ilya Kobets <i.kobets@orioledb.com>
2026-01-21 01:16:52 +0100

    Enable regular create_index test
    Also remove some changes for pg_regress and regress tests

--- src/test/regress/pg_regress.c
+++ src/test/regress/pg_regress.c
@@ -61,8 +61,8 @@ static char *shellprog = SHELLPROG;
  * Windows-style newlines, but the comparison files might or might not.
  */
 #ifndef WIN32
-const char *basic_diff_opts = "-I \"NOTICE\" -I \"DETAIL\" -I \"WARNING\"";
-const char *pretty_diff_opts = "-I \"NOTICE\" -I \"DETAIL\" -I \"WARNING\" -U3";
+const char *basic_diff_opts = "";
+const char *pretty_diff_opts = "-U3";
 #else
 const char *basic_diff_opts = "--strip-trailing-cr";
 const char *pretty_diff_opts = "--strip-trailing-cr -U3";
