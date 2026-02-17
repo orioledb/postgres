61886c61337ac6ac7c41b8a16ef47284cb7c55c8
Pavel Borisov <pashkin.elfe@gmail.com>
2026-02-17 16:08:40 +0400

    Fix merge with 3fabfc6595ced067a2
    Apply changes by 3fabfc6595ced067a2 in ExecOnConflictUpdate to heapam_tuple_insert_with_arbiter

--- src/backend/access/heap/heapam_handler.c
+++ src/backend/access/heap/heapam_handler.c
@@ -508,14 +508,6 @@ heapam_tuple_insert_with_arbiter(ResultRelInfo *resultRelInfo,
 									errmsg("could not serialize access due to concurrent update")));
 
 						/*
-						 * As long as we don't support an UPDATE of INSERT ON CONFLICT for
-						* a partitioned table we shouldn't reach to a case where tuple to
-						* be lock is moved to another partition due to concurrent update
-						* of the partition key.
-						*/
-						Assert(!ItemPointerIndicatesMovedPartitions(&tmfd.ctid));
-
-						/*
 						 * Tell caller to try again from the very start.
 						*
 						* It does not make sense to use the usual EvalPlanQual() style
@@ -532,7 +524,6 @@ heapam_tuple_insert_with_arbiter(ResultRelInfo *resultRelInfo,
 									errmsg("could not serialize access due to concurrent delete")));
 
 						/* see TM_Updated case */
-						Assert(!ItemPointerIndicatesMovedPartitions(&tmfd.ctid));
 						ExecClearTuple(lockedSlot);
 						return false;
 
