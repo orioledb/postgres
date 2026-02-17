7c7e845ec4ce47456dd21363313416dd7bb857d4
Pavel Borisov <pashkin.elfe@gmail.com>
2026-02-17 16:50:56 +0400

    Fix merge with 1d7b02711f70f1ae87b
    Change CacheInvalidateRelcacheByDbidRelid() in the same way with other CacheInvalidateRelcacheBy*()

--- src/backend/utils/cache/inval.c
+++ src/backend/utils/cache/inval.c
@@ -1472,8 +1472,6 @@ CacheInvalidateRelcacheByDbidRelid(Oid dbid, Oid relid)
 {
 	SharedInvalidationMessage msg;
 
-	PrepareInvalidationState();
-
 	msg.rc.id = SHAREDINVALRELCACHE_ID;
 	msg.rc.dbId = dbid;
 	msg.rc.relId = relid;
