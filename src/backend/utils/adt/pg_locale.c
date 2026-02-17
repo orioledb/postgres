c9e8e4ffa090a350f4a1e517941b27351148fbf0
Pavel Borisov <pashkin.elfe@gmail.com>
2026-02-17 18:22:14 +0400

    Try to fix assert in lookup_collation_cache()
    Assert added by bcb784e7d2f9 doesn't make up for calls lookup_collation_cache()
    from OrioleDB recovery

--- src/backend/utils/adt/pg_locale.c
+++ src/backend/utils/adt/pg_locale.c
@@ -69,6 +69,7 @@
 #include "utils/pg_locale.h"
 #include "utils/relcache.h"
 #include "utils/syscache.h"
+#include "access/xlog.h"
 
 #ifdef USE_ICU
 #include <unicode/ucnv.h>
@@ -1224,7 +1225,8 @@ lookup_collation_cache(Oid collation, bool set_flags)
 	Assert(OidIsValid(collation));
 	Assert(collation != DEFAULT_COLLATION_OID);
 
-	AssertCouldGetRelation();
+	if (!RecoveryInProgress())
+		AssertCouldGetRelation();
 
 	if (collation_cache == NULL)
 	{
