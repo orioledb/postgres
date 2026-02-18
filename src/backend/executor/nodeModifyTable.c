af485c25ccd0ec91a957554cfb1eb9e3babbea5e
Alexander Korotkov <akorotkov@postgresql.org>
2026-02-18 14:18:17 +0200

    Modify the way ExecMergeMatched() detects moved partitions
    Use tupleid for ROW_REF_TID as in upsteam.  Use &context->tmfd.ctid for
    ROW_REF_ROWID as in OrioleDB.

--- src/backend/executor/nodeModifyTable.c
+++ src/backend/executor/nodeModifyTable.c
@@ -3131,6 +3131,7 @@ lmerge_matched:
 					bool		was_matched;
 					Relation	resultRelationDesc;
 					TupleTableSlot *epqslot;
+					ItemPointer movedPartitionCheckCtid;
 
 					if (IsolationUsesXactSnapshot())
 						ereport(ERROR,
@@ -3161,7 +3162,11 @@ lmerge_matched:
 					 * the tuple moved, and setting our current
 					 * resultRelInfo to that.
 					 */
-					if (ItemPointerIndicatesMovedPartitions(&context->tmfd.ctid))
+					if (resultRelInfo->ri_RowRefType == ROW_REF_TID)
+						movedPartitionCheckCtid = (ItemPointer) tupleid;
+					else
+						movedPartitionCheckCtid = &context->tmfd.ctid;
+					if (ItemPointerIndicatesMovedPartitions(movedPartitionCheckCtid))
 						ereport(ERROR,
 								(errcode(ERRCODE_T_R_SERIALIZATION_FAILURE),
 								 errmsg("tuple to be merged was already moved to another partition due to concurrent update")));
