18e1f7934aa9ed78dc5f65e8f922aa4254dff017
Pavel Borisov <pashkin.elfe@gmail.com>
2026-05-20 19:10:01 +0400

    Revert "Add verify_tableam table AM method to support amcheck on Orioledb tables"
    This reverts commit 9619f2a58655819a858d95fca4a6ef9bbe11b2b0.

--- src/bin/pg_amcheck/pg_amcheck.c
+++ src/bin/pg_amcheck/pg_amcheck.c
@@ -1876,30 +1876,28 @@ compile_relation_list_one_db(PGconn *conn, SimplePtrList *relations,
 		appendPQExpBufferStr(&sql, ") NULL::INTEGER AS pattern_id,");
 	appendPQExpBuffer(&sql,
 					  "\nc.oid, n.nspname, c.relname, c.reltoastrelid, c.relpages, "
-					  "am.amtype = 't' AS is_heap, "
+					  "c.relam = %u AS is_heap, "
 					  "c.relam = %u AS is_btree"
 					  "\nFROM pg_catalog.pg_class c "
 					  "INNER JOIN pg_catalog.pg_namespace n "
-					  "ON c.relnamespace = n.oid "
-					  "LEFT JOIN pg_catalog.pg_am am "
-					  "ON am.oid = c.relam",
-					  BTREE_AM_OID);
+					  "ON c.relnamespace = n.oid",
+					  HEAP_TABLE_AM_OID, BTREE_AM_OID);
 	if (!opts.allrel)
 		appendPQExpBuffer(&sql,
 						  "\nINNER JOIN include_pat ip"
 						  "\nON (n.nspname ~ ip.nsp_regex OR ip.nsp_regex IS NULL)"
 						  "\nAND (c.relname ~ ip.rel_regex OR ip.rel_regex IS NULL)"
-						  "\nAND (am.amtype = 't' OR NOT ip.heap_only)"
+						  "\nAND (c.relam = %u OR NOT ip.heap_only)"
 						  "\nAND (c.relam = %u OR NOT ip.btree_only)",
-						  BTREE_AM_OID);
+						  HEAP_TABLE_AM_OID, BTREE_AM_OID);
 	if (opts.excludetbl || opts.excludeidx || opts.excludensp)
 		appendPQExpBuffer(&sql,
 						  "\nLEFT OUTER JOIN exclude_pat ep"
 						  "\nON (n.nspname ~ ep.nsp_regex OR ep.nsp_regex IS NULL)"
 						  "\nAND (c.relname ~ ep.rel_regex OR ep.rel_regex IS NULL)"
-						  "\nAND (am.amtype = 't' OR NOT ep.heap_only OR ep.rel_regex IS NULL)"
+						  "\nAND (c.relam = %u OR NOT ep.heap_only OR ep.rel_regex IS NULL)"
 						  "\nAND (c.relam = %u OR NOT ep.btree_only OR ep.rel_regex IS NULL)",
-						  BTREE_AM_OID);
+						  HEAP_TABLE_AM_OID, BTREE_AM_OID);
 
 	/*
 	 * Exclude temporary tables and indexes, which must necessarily belong to
@@ -1927,17 +1925,18 @@ compile_relation_list_one_db(PGconn *conn, SimplePtrList *relations,
 	 */
 	if (opts.allrel)
 		appendPQExpBuffer(&sql,
-						  " AND am.amtype = 't' "
+						  " AND c.relam = %u "
 						  "AND c.relkind IN ('r', 'S', 'm', 't') "
 						  "AND c.relnamespace != %u",
-						  PG_TOAST_NAMESPACE);
+						  HEAP_TABLE_AM_OID, PG_TOAST_NAMESPACE);
 	else
 		appendPQExpBuffer(&sql,
-						  " AND (am.amtype = 't' OR c.relam = %u)"
+						  " AND c.relam IN (%u, %u)"
 						  "AND c.relkind IN ('r', 'S', 'm', 't', 'i') "
-						  "AND ((am.amtype = 't' AND c.relkind IN ('r', 'S', 'm', 't')) OR "
+						  "AND ((c.relam = %u AND c.relkind IN ('r', 'S', 'm', 't')) OR "
 						  "(c.relam = %u AND c.relkind = 'i'))",
-						  BTREE_AM_OID, BTREE_AM_OID);
+						  HEAP_TABLE_AM_OID, BTREE_AM_OID,
+						  HEAP_TABLE_AM_OID, BTREE_AM_OID);
 
 	appendPQExpBufferStr(&sql,
 						 "\nORDER BY c.oid)");
@@ -1973,18 +1972,15 @@ compile_relation_list_one_db(PGconn *conn, SimplePtrList *relations,
 		 * selected above, filtering by exclusion patterns (if any) that match
 		 * btree index names.
 		 */
-		appendPQExpBuffer(&sql,
+		appendPQExpBufferStr(&sql,
 							 ", index (oid, nspname, relname, relpages) AS ("
 							 "\nSELECT c.oid, r.nspname, c.relname, c.relpages "
 							 "FROM relation r"
-							 "\nINNER JOIN pg_catalog.pg_class pc "
-							 "ON pc.oid = r.oid AND pc.relam = %u"
 							 "\nINNER JOIN pg_catalog.pg_index i "
 							 "ON r.oid = i.indrelid "
 							 "INNER JOIN pg_catalog.pg_class c "
 							 "ON i.indexrelid = c.oid "
-							 "AND c.relpersistence != 't'",
-							 HEAP_TABLE_AM_OID);
+							 "AND c.relpersistence != 't'");
 		if (opts.excludeidx || opts.excludensp)
 			appendPQExpBufferStr(&sql,
 								 "\nINNER JOIN pg_catalog.pg_namespace n "
