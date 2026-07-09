9f77e0a6744ee7eb0951a43957622cb8baf97d4e
GremSnoort <grem_snoort@protonmail.com>
2026-07-09 16:53:53 +0400

    Free temporary ROW_REF_ROWID rowid Datums after use
    Co-authored-by: Pavel Borisov <pashkin.elfe@gmail.com>

--- src/backend/executor/nodeModifyTable.c
+++ src/backend/executor/nodeModifyTable.c
@@ -2457,6 +2457,9 @@ ExecOnConflictUpdate(ModifyTableContext *context,
 
 	if (!ExecQual(onConflictSetWhere, econtext))
 	{
+		if (table_get_row_ref_type(resultRelInfo->ri_RelationDesc) == ROW_REF_ROWID &&
+			DatumGetPointer(tupleid) != NULL)
+			pfree(DatumGetPointer(tupleid));
 		ExecClearTuple(existing);	/* see return below */
 		InstrCountFiltered1(&mtstate->ps, 1);
 		return true;			/* done with the tuple */
@@ -2502,6 +2505,10 @@ ExecOnConflictUpdate(ModifyTableContext *context,
 							resultRelInfo->ri_onConflict->oc_ProjSlot,
 							existing,
 							canSetTag, true);
+
+	if (table_get_row_ref_type(resultRelInfo->ri_RelationDesc) == ROW_REF_ROWID &&
+		DatumGetPointer(tupleid) != NULL)
+		pfree(DatumGetPointer(tupleid));
 
 	/*
 	 * Clear out existing tuple, as there might not be another conflict among
