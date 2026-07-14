68b634bd8888eac29959806eeaf6e872fa1bed7b
Elizaveta Reutt <svetozarova@yandex.ru>
2026-07-14 09:20:57 +0300

    Fix misprint declaration of usercache callbacks buffer

--- src/backend/utils/cache/inval.c
+++ src/backend/utils/cache/inval.c
@@ -306,7 +306,7 @@ static struct USERCACHECALLBACK
 {
 	UsercacheCallbackFunction function;
 	Datum		arg;
-}			usercache_callback_list[MAX_RELCACHE_CALLBACKS];
+}			usercache_callback_list[MAX_USERCACHE_CALLBACKS];
 
 static int	usercache_callback_count = 0;
 
