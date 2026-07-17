f07b9c5087f4222bbb7bdc6ac35d5817c246ab93
Nick Babadzhanian <33933459+pgnickb@users.noreply.github.com>
2026-07-17 09:36:43 +0200

    Fix set_plain_rel_pathlist path
    * Fix set_plain_rel_pathlist path
    * Adjust hook function signature
    
    Previously addition of SeqScan to the pathlist depended on the return value of the set_plain_rel_pathlist hook. This would result in SeqScan not being considered in certain cases where it could be useful.

--- src/backend/optimizer/path/allpaths.c
+++ src/backend/optimizer/path/allpaths.c
@@ -773,10 +773,11 @@ set_plain_rel_pathlist(PlannerInfo *root, RelOptInfo *rel, RangeTblEntry *rte)
 	 */
 	required_outer = rel->lateral_relids;
 
-	if (!set_plain_rel_pathlist_hook ||
-		set_plain_rel_pathlist_hook(root, rel, rte))
-		/* Consider sequential scan */
-		add_path(rel, create_seqscan_path(root, rel, required_outer, 0));
+	if (set_plain_rel_pathlist_hook)
+		set_plain_rel_pathlist_hook(root, rel, rte);
+
+	/* Consider sequential scan */
+	add_path(rel, create_seqscan_path(root, rel, required_outer, 0));
 
 	/* If appropriate, consider parallel sequential scan */
 	if (rel->consider_parallel && required_outer == NULL)
