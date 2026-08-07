e9b0fcd48e095eb1b95c4ef26081270cef4ed7c2
Elizaveta Reutt <svetozarova@yandex.ru>
2026-08-07 15:18:54 +0300

    invoke RecoveryInProgress outside SpinLock in GetEffectiveXlogReplayRecPtr

--- src/backend/access/transam/xlogrecovery.c
+++ src/backend/access/transam/xlogrecovery.c
@@ -4660,9 +4660,10 @@ XLogRecPtr
 GetEffectiveXlogReplayRecPtr(void)
 {
 	XLogRecPtr	recptr = InvalidXLogRecPtr;
+	bool		useHook = GetReplayXlogPtrHook != NULL && RecoveryInProgress();
 
 	SpinLockAcquire(&XLogRecoveryCtl->info_lck);
-	if (GetReplayXlogPtrHook)
+	if (useHook)
 		recptr = GetReplayXlogPtrHook();
 	if (recptr == InvalidXLogRecPtr)
 		recptr = XLogRecoveryCtl->lastReplayedEndRecPtr;
